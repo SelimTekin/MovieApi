2d6af57f617ac0cc802e79226784655254ad81cc
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-04-14 19:19:25 +0300

    UI Layoout

---
+++ Frontends/MovieApi.WebUI/UserLayoutWebUIViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUIHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.UserLayoutWebUIViewComponents.UserLayoutWebUIViewComponents
+{
+    public class _UserLayoutWebUIHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
