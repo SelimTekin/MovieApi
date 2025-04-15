ed2809575c4247e48d5715e8c063900dcf8ff493
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-04-15 19:19:23 +0300

    ViewComponents

---
+++ Frontends/MovieApi.WebUI/UserLayoutWebUIViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUINavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.UserLayoutWebUIViewComponents.UserLayoutWebUIViewComponents
+{
+    public class _UserLayoutWebUINavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
